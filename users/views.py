63eb39e8b4f7d913f71b3fc8777ee75f32bf9d97
YohanRameau <yohan.rameau@gmail.com>
2021-03-14 12:31:18 +0100

    Finish login and logout features

--- users/views.py
+++ users/views.py
@@ -11,8 +11,8 @@ def register(request: HttpRequest):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username} !")
-            return redirect('task:index')
+            messages.success(request, f"Your account has been created ! You are now able to login.")
+            return redirect('login')
     else:
-        form = UserRegisterForm()
+        form = UserRegisterForm() 
     return render(request, 'users/register.html', {'form': form})
